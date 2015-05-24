7844d47ea8f909ff2909422bf789c5bd204fdb4d
Andy Baird <Andy.Baird@icloud.com>
2015-05-23 20:09:11 -0400

    output is working again, displays 'object'

--- js/app-user.js
+++ js/app-user.js
@@ -1,19 +1,15 @@
 app.userList = {
   all: [],
-
   add: function (user) {
     this.all.push(user);
     this.save();
   },
-
   remove: function (index) {
   },
 
   load: function () {
     this.all = JSON.parse(localStorage.getItem('__')) || [];
-
   },
-
   save: function () {
     localStorage.setItem('___', JSON.stringify(this.all));
   }
--- js/functions.js
+++ js/functions.js
@@ -6,17 +6,12 @@ $(function () {
     var firstName = $("#firstName").val();
     var lastName = $("#lastName").val();
     var eMail = $("#eMail").val();
-    var userHash = {
-      firstname: firstName,
-      lastname: lastName,
-      email: eMail
-    };
 
-    app.userList.add(userHash);
 
-    console.log(app.userList.all);
+    app.userList.add(firstName, lastName, eMail);
+
+    app.renderUser(app.userList.all);
 
     $(".userInfo").val('');
-    return userHash;
   });
 });
